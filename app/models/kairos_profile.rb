eac3afd497183e2309b3a2d8cf05f7cc38523e79
jciancio-carecloud <jciancio@carecloud.com>
2017-06-10 18:37:19 -0400

    add route and spike out endpoint for adding a kairos profile to user

--- app/models/kairos_profile.rb
+++ app/models/kairos_profile.rb
@@ -1,3 +1,4 @@
 class KairosProfile < ApplicationRecord
   belongs_to :user
+  validates :user, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
 
   resources :users, only: :index do
+    resources :kairos_profile, only: :create
     resources :user_likes, path: 'likes', shallow: true do
       resources :reaction_data, shallow: true
     end
