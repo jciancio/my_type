d6cd39652b0a8a1ed61676d8d5d4ef4df4fefc87
jciancio-carecloud <jciancio@carecloud.com>
2017-06-10 20:18:59 -0400

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   post '/login' => 'sessions#create'
 
-  resources :users, only: :index do
-    resources :kairos_profile, only: :create
-    resources :user_likes, path: 'likes', shallow: true do
-      resources :reaction_data, shallow: true
-    end
+  resources :users, only: [:index, :show]
+
+  resources :kairos_profile, only: :create
+
+  resources :user_likes, path: 'likes', only: [:index, :create], shallow: true do
+    resources :reaction_data, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
